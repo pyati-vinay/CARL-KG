b70f1e19a4b8dc5ab2bcf5fae0a18001920ebcb1
Vinay Pyati <vinay.pyati@rwth-aachen.de>
2018-08-22 04:11:54 +0200

    adjust code length

--- src/main/scala/com/kg/carl/Algorithm.scala
+++ src/main/scala/com/kg/carl/Algorithm.scala
@@ -249,7 +249,8 @@ object Algorithm {
                   var expectedComplete = 0
                   if (expectedTriplePerRelation.contains(rule.r)) {
                     expectedIncomplete = expectedTriplePerRelation(rule.r) / numPossibleRelations
-                    expectedComplete = (numPossibleRelations - expectedTriplePerRelation(rule.r) - propertyInstances(rule.r)) / numPossibleRelations
+					val tmp = (numPossibleRelations - expectedTriplePerRelation(rule.r) - propertyInstances(rule.r))
+                    expectedComplete =  tmp / numPossibleRelations
                   }
                   val complete = tripleAddedToCompletePlaces.asInstanceOf[Double] / rule.bodySupport
                   val incomplete = tripleAddedToMissingPlaces.asInstanceOf[Double] / rule.bodySupport
